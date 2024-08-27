32463b1d15838cc4a4a2be828bebbf75544dba8d
Stephen <stephen.jacob@tus.ie>
2024-08-27 09:06:38 +0000

    Added MatSort, filterPredicate elements to filter issies by name and affected entity.

--- crusoe_orient/src/app/panels/issues/issue.component.ts
+++ crusoe_orient/src/app/panels/issues/issue.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild, AfterViewInit, ElementRef, ChangeDetectorRef } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
 import { zip } from 'rxjs';
 import { Issue, issues } from 'src/app/app.data';
 
@@ -16,10 +17,30 @@ export class IssueComponent implements OnInit, AfterViewInit {
   displayedColumns: string[] = ['name', 'severity', 'status', 'affected_entity', 'description'];
   
   @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
   
   ngOnInit(): void {}
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
-  }  
+    this.dataSource.sort = this.sort;
+
+    this.dataSource.filterPredicate = (data: Issue, filter: string) => {
+      const filterValue = filter.trim().toLowerCase();
+
+      return (
+	data.name.trim().toLowerCase().includes(filterValue) ||
+	data.affected_entity.trim().toLowerCase().includes(filterValue)
+      );
+    };
+  }
+
+  applyFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
