b47bdfa35fddd2749ea8bfdc9e8c423781e0310a
fjallnari <draconirx@gmail.com>
2025-08-15 13:56:49 +0200

    Redesign network nodes page with Sentinel card & badges

--- _update/src/app/pages/network-page/entities.config.ts
+++ _update/src/app/pages/network-page/entities.config.ts
@@ -1,5 +1,6 @@
 type EntityKey = {
   bgColor: string,
+  textColor?: string,
   showProperty: string[]
 }
 
@@ -17,34 +18,42 @@ export type EntityStructure = {
 export const entities: EntityStructure = {
   IP: {
     bgColor: '#FF9800',
+    textColor: '#000',
     showProperty: ['address'],
   },
   DomainName: {
     bgColor: '#19AADE',
+    textColor: '#000',
     showProperty: ['domain_name'],
   },
   Subnet: {
     bgColor: '#6877CA',
+    textColor: '#fff',
     showProperty: ['range'],
   },
   Node: {
     bgColor: '#DC4141',
+    textColor: '#fff',
     showProperty: ['topology_betweenness'],
   },
   SecurityEvent: {
     bgColor: '#009688',
+    textColor: '#fff',
     showProperty: ['type'],
   },
   CVE: {
     bgColor: '#9C27B0',
+    textColor: '#fff',
     showProperty: ['CVE_id'],
   },
   Vulnerability: {
     bgColor: '#FF5722',
+    textColor: '#fff',
     showProperty: ['name'],
   },
   SoftwareVersion: {
     bgColor: '#8BC34A',
+    textColor: '#fff',
     showProperty: ['version'],
   },
 };
